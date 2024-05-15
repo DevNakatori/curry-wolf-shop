50cfe0fcd5db37c127b3fbdfc5b099d7903cd2ad
DevNakatori <dev@nakatori.agency>
2024-05-15 16:11:35 +0530

    video animation added

--- app/assets/js/homepage.js
+++ app/assets/js/homepage.js
@@ -1,10 +1,27 @@
-window.addEventListener('scroll', function() {
-    var header = document.querySelector('.header');
-    var scrollPosition = window.scrollY;
 
-    if (scrollPosition > 50) {
-        header.classList.add('header-scrolled');
-    } else {
-        header.classList.remove('header-scrolled');
-    }
-});
+const overlay = document.querySelector('.video-overlay');
+const text1 = document.getElementById('text1');
+const text2 = document.getElementById('text2');
+const video = document.getElementById('bgVideo');
+const handleClickText1 = () => {
+    hideElements([overlay, text1]);
+    video.removeAttribute('loop');
+};
+const handleVideoEnd = () => {
+    showElements([overlay, text2]);
+    video.setAttribute('loop', 'loop');
+    video.play();
+};
+text1.addEventListener('click', handleClickText1);
+video.addEventListener('ended', handleVideoEnd);
+function hideElements(elements) {
+    elements.forEach(element => {
+        element.classList.add('hidden');
+    });
+}
+function showElements(elements) {
+    elements.forEach(element => {
+        element.classList.remove('hidden');
+    });
+}
+
