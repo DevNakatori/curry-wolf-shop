b314a352af22135dbde08a9af8fb5a38c7e7e5fa
DevNakatori <dev@nakatori.agency>
2024-05-15 19:52:00 +0530

    404 resolved

--- app/assets/js/homepage.js
+++ app/assets/js/homepage.js
@@ -1,4 +1,3 @@
-// Define variables for overlay, text elements, and video
 const overlay = document.querySelector('.video-overlay');
 const text1 = document.getElementById('text1');
 const text2 = document.getElementById('text2');
@@ -13,40 +12,33 @@ const hideElementsAfterDelay = () => {
     }, 2000);
 };
 
-// Function to handle video end event
 const handleVideoEnd = () => {
     showElements([overlay, text2]);
     video.setAttribute('loop', 'loop');
     video.play();
-    removeTransparentClass(header); // Remove transparent class when video ends
+    removeTransparentClass(header); 
 };
 
-// Call function to hide elements after delay
 hideElementsAfterDelay();
 
-// Set up event listener for video end
 video.addEventListener('ended', handleVideoEnd);
 
-// Helper function to hide multiple elements
 function hideElements(elements) {
     elements.forEach(element => {
         element.classList.add('hidden');
     });
 }
 
-// Helper function to show multiple elements
 function showElements(elements) {
     elements.forEach(element => {
         element.classList.remove('hidden');
     });
 }
 
-// Helper function to add transparent class to an element
 function addTransparentClass(element) {
     element.classList.add('transparent');
 }
 
-// Helper function to remove transparent class from an element
 function removeTransparentClass(element) {
     element.classList.remove('transparent');
 }
