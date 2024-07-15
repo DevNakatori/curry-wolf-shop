31ce927fd9f8d2ba8770aaa6d975a7ef6873880b
darshan-zinzuvadiya <dzinzuvadiya2699@gmail.com>
2024-07-15 11:10:12 +0530

    collection inner pages created

--- app/routes/($locale).pages.potsdam.jsx
+++ app/routes/($locale).pages.potsdam.jsx
@@ -1,10 +1,6 @@
 import {json} from '@shopify/remix-oxygen';
 import {useLoaderData} from '@remix-run/react';
-import Contact from '~/components/ContactForm';
-import React, { useEffect } from "react";
-import '../styles/contact-page.css';
-import { loadScript } from '@shopify/hydrogen-react/load-script';
-
+import '../styles/location-inner.css';
 /**
  * @type {MetaFunction<typeof loader>}
  */
@@ -19,7 +15,7 @@ export async function loader({params, context}) {
 //   if (!params.handle) {
 //     throw new Error('Missing page handle');
 //   }
-  const handle = params.handle || 'contact-new';
+  const handle = params.handle || 'potsdam';
   const {page} = await context.storefront.query(PAGE_QUERY, {
     variables: {
       handle: handle,
@@ -31,26 +27,16 @@ export async function loader({params, context}) {
   }
 
   return json({page});
+
 }
 
 export default function Page() {
-  useEffect(() => {
-    LoadBokunScript();
-  }, []);
-
-  function LoadBokunScript() {
-      loadScript(`//static.klaviyo.com/onsite/js/klaviyo.js?Fcompany_id=WuBDFR`).catch(() => {
-        console.log("klavidooo");
-      });
-}
   /** @type {LoaderReturnData} */
   const {page} = useLoaderData();
 
   return (
-    <div className="page contact-page">
-      <>
+    <div className="page job-detail-main">
       <main dangerouslySetInnerHTML={{__html: page.body}} />
-      </>
     </div>
   );
 }
