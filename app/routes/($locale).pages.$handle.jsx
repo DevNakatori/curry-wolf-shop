f1bea644089c2091dbd58d9cc8ae73f224674c0f
DevNakatori <dev@nakatori.agency>
2024-07-25 16:26:33 +0530

    css update

--- app/routes/($locale).pages.$handle.jsx
+++ app/routes/($locale).pages.$handle.jsx
@@ -38,7 +38,7 @@ export default function Page() {
       <header>
         <h1>{page.title}</h1>
       </header>
-      <main dangerouslySetInnerHTML={{__html: page.body}} />
+      <p dangerouslySetInnerHTML={{__html: page.body}} /></p>
     </div>
   );
 }
